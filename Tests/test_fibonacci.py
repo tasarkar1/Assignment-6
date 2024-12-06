620a9ec2b6604294d40cd346652046e836b66e6c
tasarkar1 <timsarkar@gmail.com>
2024-12-06 16:53:51 -0500

    Test 4

--- Tests/test_fibonacci.py
+++ Tests/test_fibonacci.py
@@ -1,7 +1,7 @@
 import pytest
 from fibonacci import Fibonacci
 
-def test_constructor_one():
-    assert (list(Fibonacci(1)))== [0,1]
+def test_constructor_two():
+    assert (list(Fibonacci(2)))== [0,1,1]
 
 
